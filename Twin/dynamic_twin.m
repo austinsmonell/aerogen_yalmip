868c44ce203eebb45826932bd7244130347db19b
austinmonell <austmonell67@gmail.com>
2025-03-26 02:50:45 -0600

    created twin kite config directory and copied single kite files over

---
+++ Twin/dynamic_twin.m
@@ -0,0 +1,75 @@
+function dx = dynamic_twin(x, u, p)
+%% extract variables
+% States: theta, theta_dot
+sigma = x(1,:);
+sigma_dot = x(2,:);
+va1_xy = x(3, :);
+theta1 = x(4, :);
+psi1 = x(5, :);
+x1 = x(6, :);
+y1 = x(7, :);
+% theta1_dot = x(8, :);
+
+%Controls: dist1
+m_ctr = u(1, :);
+de1 = u(2, :);
+ds1 = u(3, :);
+dr1 = u(4, :);
+
+% extract parameters
+r_gen = p(1);
+moi_g = p(2);
+d = p(5);
+m_ac = p(6);
+rho = p(7);
+S = p(8);
+CL0 = p(9);
+CLa = p(10);
+CD0 = p(11);
+e = p(12);
+vw = p(13);
+moi_ac_m = p(14);
+d_c = p(15);
+d_b = S/d_c;
+CMa = p(16);
+CMq = p(17);
+CMde = p(18);
+CNb = p(19);
+CNr = p(20);
+CNdr = p(21);
+CYb = p(22);
+moi_ac_n = p(23);
+CDb = p(24);
+g = p(25);
+m_teth = p(26);
+CD_eff_teth = p(27);
+CDds = p(29);
+omega = p(30);
+%% dynamics
+r1_dot = sigma_dot*r_gen;%done
+va1_r = -r1_dot+vw;%done
+alpha1 = theta1+atan2(va1_r, va1_xy);%done
+
+q1 = 0.5*rho*(va1_r.^2+va1_xy.^2);%done
+Cl1 = (CL0+CLa*alpha1);%done
+L1 = q1*S.*Cl1;%done
+Cd1 = (CD0+CD_eff_teth+e*Cl1.^2+CDds*ds1);
+D1 = q1*S.*Cd1;%done
+F1_zb = -sin(alpha1).*D1 - cos(alpha1).*L1;%done
+F1_xb = -cos(alpha1).*D1 + sin(alpha1).*L1;%done
+
+% M1 = q1*d_c*S.*(CMa*alpha1+CMq*theta1_dot+CMde*de1);
+
+x1_dot = va1_xy.*cos(psi1);
+y1_dot = va1_xy.*sin(psi1);
+psi1_dot = dr1;
+% theta1_dot_dot = M1/moi_ac_m;
+theta1_dot = de1;
+vxy_dot = (F1_xb.*cos(theta1)+ F1_zb.*sin(theta1)-(m_ac+m_teth/2-omega*rho)*g*sin(psi1))/(m_ac+m_teth/3);
+
+F1_aero_r = F1_xb.*(-sin(theta1))+F1_zb.*(cos(theta1));%done
+
+sigma_dot_dot = (-F1_aero_r*r_gen+m_ctr)/(moi_g+(m_ac+m_teth)*r_gen^2);
+%% package derivitives
+dx = [sigma_dot; sigma_dot_dot; vxy_dot; theta1_dot; psi1_dot; x1_dot; y1_dot];%; theta1_dot_dot];
+end
---
+++ Twin/plot_aerogen_twin.m
@@ -0,0 +1,106 @@
+function  plot_aerogen_twin(x,u,p,time)
+    %% Generator
+    plot_rows = 5; plot_cols = 2;fig_val = 1;
+    figure
+
+    subplot(plot_rows, plot_cols, fig_val)
+    plot(time, x(1, :)/(2*pi))
+    title('Sigma')
+    xlabel('Time [s]')
+    ylabel('Sigma [revs]')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    plot(time, x(2, :)/(2*pi))
+    title('Sigma dot')
+    xlabel('Time [s]')
+    ylabel('Sigma dot [rev/s]')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    hold on
+    plot(time, x(6, :))
+    plot(time, x(7, :))
+    title('Position')
+    xlabel('Time [s]')
+    ylabel('Position [m]')
+    legend('X', 'Y')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    hold on
+    plot(time, x(3, :))
+    title('Speed')
+    xlabel('Time [s]')
+    ylabel('Va [mps]')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    hold on
+    plot(time, x(5, :)*180/pi)
+    title('Heading')
+    xlabel('Time [s]')
+    ylabel('Psi [deg]')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    hold on
+    plot(time, u(4, :))
+    plot(time, u(3, :))
+    title('Control')
+    xlabel('Time [s]')
+    legend('dr', 'ds')
+    fig_val = fig_val+1;
+
+    sigma_dot = x(2, :);
+    r_gen = p(1);
+    vw = p(13);
+    theta1 = x(4, :);
+    va1_xy = x(3, :);
+    r1_dot = sigma_dot*r_gen;
+    va1_r = -r1_dot+vw;
+    alpha1 = theta1+atan2(va1_r, va1_xy);%done
+
+    subplot(plot_rows, plot_cols, fig_val)
+    hold on
+    plot(time, x(4, :)*180/pi)
+    plot(time, alpha1*180/pi)
+    title('Attitude')
+    xlabel('Time [s]')
+    ylabel('Pitch [deg]')
+    legend('pitch', 'alpha')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    hold on
+    plot(time, u(2, :))
+    title('Control [de]')
+    xlabel('Time [s]')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    plot(time, u(1, :))
+    title('Gen Moment')
+    xlabel('Time [s]')
+    ylabel('Gen Moment [Nm]')
+    fig_val = fig_val+1;
+
+    subplot(plot_rows, plot_cols, fig_val)
+    plot(time, (-u(1, :).*x(2, :))/1000)
+    title('Objective')
+    xlabel('Time [s]')
+    ylabel('Power [kW]')
+    fig_val = fig_val+1;
+
+  
+
+    %% 3D Plot
+    figure
+    plot3(x(6, :), x(7, :), x(1, :)*p(1))
+    axis equal 
+    title('3D')
+    xlabel('X [m]')
+    ylabel('Y [m]')
+
+
+end
