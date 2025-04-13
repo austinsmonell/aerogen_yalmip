485062f5e3689d35033c876bb68f8ac1321e3f7b
austinmonell <austmonell67@gmail.com>
2025-04-13 01:44:57 -0600

    [Twin & Single] created ctr_obj_gain to control about ctr rates effect objective, moved alpha lim to top of script, added wind speed to runs scripts, [Single] added intial range for psi

--- Single/run_aerogen_single.m
+++ Single/run_aerogen_single.m
@@ -13,13 +13,16 @@ tf = 10;
 gridSz = 60;
 dt = tf/(gridSz-1);
 timeVec = linspace(0, tf, gridSz);
+ctr_obj_gain = 10;
+alpha_lim = 30*pi/180;
+wind_spd = 12;
 
 % Define variables/params
 nx = 7; 
 nu = 3; 
 x = sdpvar(nx,gridSz);%1:sigma 2:sigma_dot, 3:va, 4: theta, 5:psi, 6:x1, 7:x2
 u = sdpvar(nu, gridSz);%1:m_ctr, 2:de1(theta_dot), 3:dr1(psi_Dot)
-p = getParams(); p(31) = tf; p(32) = dt;
+p = getParams(); p(31) = tf; p(32) = dt; p(13) = wind_spd;
 if use_guess
     load(strcat(save_name, '_states.mat'));
     load(strcat(save_name, '_ctrs.mat'));
@@ -32,8 +35,8 @@ u_up = [0; 1; 1];
 u_lw = [-1; -1; -1];
 x_up = [inf;  inf; 100; pi/4; inf; inf; inf];
 x_lw = [-inf; -inf; 1; -pi/4; -inf; -inf; -inf];
-x0_up = [0; inf; 100; pi/4; 0; 0; 0];
-x0_lw = [0; 0; 1; -pi/4; 0;  0; 0];
+x0_up = [0; inf; 100; pi/4; pi; 0; 0];
+x0_lw = [0; 0; 1; -pi/4; -pi;  0; 0];
 
 %% Contraints & Objective
 Constraints = [];
@@ -51,7 +54,6 @@ r_gen = p(1);
 vw = p(13);
 theta1 = x(4, :);
 va1_xy = x(3, :);
-alpha_lim = 30*pi/180;
 
 r1_dot = sigma_dot*r_gen;
 va1_r = -r1_dot+vw;
@@ -62,7 +64,7 @@ Constraints = [Constraints, u<=ones(nu, gridSz).*u_up, u>=ones(nu, gridSz).*u_lw
 
 %dynamics and objective
 cyl_idx = [2, 3, 4, 6, 7];
-Objective = u(1, 1).*x(2, 1)+sum(abs(u(:, 1)-u(:, end)))*10;
+Objective = u(1, 1).*x(2, 1)+sum(abs(u(:, 1)-u(:, end)))*ctr_obj_gain;
 x_dotk = dynamic_single(x(:, end), u(:, end), p);
 x_dotk1 = dynamic_single(x(:, 1), u(:, 1), p);
 Constraints = [Constraints, x(cyl_idx, 1) == x(cyl_idx, end)+dt/2*(x_dotk(cyl_idx)+x_dotk1(cyl_idx)),...
@@ -71,7 +73,7 @@ for m = 1 : gridSz-1
   x_dotk = dynamic_single(x(:, m), u(:, m), p);
   x_dotk1 = dynamic_single(x(:, m+1), u(:, m+1), p);
   Constraints = [Constraints, x(:, m+1) == x(:, m)+dt/2*(x_dotk+x_dotk1)];%discrete time dynamics, equality cnst
-  Objective = Objective + u(1, m+1).*x(2, m+1)+sum(abs(u(:, m+1)-u(:, m)))*10; %objective func, max energy
+  Objective = Objective + u(1, m+1).*x(2, m+1)+sum(abs(u(:, m+1)-u(:, m)))*ctr_obj_gain; %objective func, max energy
 end
 
 % Set some options for YALMIP and solver
--- Twin/run_aerogen_twin.m
+++ Twin/run_aerogen_twin.m
@@ -13,13 +13,16 @@ tf = 10;
 gridSz = 60;
 dt = tf/(gridSz-1);
 timeVec = linspace(0, tf, gridSz);
+ctr_obj_gain = 10;
+alpha_lim = 30*pi/180;
+wind_spd = 12;
 
 % Define variables/params
 nx = 12; 
 nu = 5;
 x = sdpvar(nx,gridSz);%1:sigma 2:sigma_dot, 3:theta1, 4:theta2, 5:va1, 6:va2, 7:psi1, 8:psi2, 9:x1, 10:y1, 11:x2, 12:y2
 u = sdpvar(nu, gridSz);%1:m_ctr, 2:de1(theta1_dot), 3:de2(theta2_dot), 4:dr1(psi1_dot), 4:dr2(psi2_dot)
-p = getParams(); p(31) = tf; p(32) = dt;
+p = getParams(); p(31) = tf; p(32) = dt; p(13) = wind_spd;
 if use_guess
     load(strcat(save_name, '_states.mat'));
     load(strcat(save_name, '_ctrs.mat'));
@@ -53,7 +56,6 @@ theta1 = x(3, :);
 va1_xy = x(5, :);
 theta2 = x(4, :);
 va2_xy = x(6, :);
-alpha_lim = 30*pi/180;
 
 r1_dot = sigma_dot*r_gen;
 va1_r = -r1_dot+vw;
@@ -66,7 +68,7 @@ Constraints = [Constraints, u<=ones(nu, gridSz).*u_up, u>=ones(nu, gridSz).*u_lw
 
 %dynamics and objective
 cyl_idx = [1, 2, 3, 4, 5, 6, 9, 10, 11, 12];
-Objective = u(1, 1).*x(2, 1)+sum(abs(u(:, 1)-u(:, end)))*10;
+Objective = u(1, 1).*x(2, 1)+sum(abs(u(:, 1)-u(:, end)))*ctr_obj_gain;
 x_dotk = dynamic_twin(x(:, end), u(:, end), p);
 x_dotk1 = dynamic_twin(x(:, 1), u(:, 1), p);
 Constraints = [Constraints, x(cyl_idx, 1) == x(cyl_idx, end)+dt/2*(x_dotk(cyl_idx)+x_dotk1(cyl_idx)),...
@@ -76,7 +78,7 @@ for m = 1 : gridSz-1
   x_dotk = dynamic_twin(x(:, m), u(:, m), p);
   x_dotk1 = dynamic_twin(x(:, m+1), u(:, m+1), p);
   Constraints = [Constraints, x(:, m+1) == x(:, m)+dt/2*(x_dotk+x_dotk1)];%discrete time dynamics, equality cnst
-  Objective = Objective + u(1, m+1).*x(2, m+1)+sum(abs(u(:, m+1)-u(:, m)))*10; %objective func, max energy
+  Objective = Objective + u(1, m+1).*x(2, m+1)+sum(abs(u(:, m+1)-u(:, m)))*ctr_obj_gain; %objective func, max energy
 end
 
 % Set some options for YALMIP and solver
